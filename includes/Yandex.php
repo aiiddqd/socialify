4dff21d665ba4fbed594d031473c80bb098d73dd
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-12-08 10:59:02 +0300

    dd

--- includes/Yandex.php
+++ includes/Yandex.php
@@ -38,61 +38,128 @@ final class Yandex
     {
         $appConfig = self::get_config();
 
-        if(empty($appConfig['id']) || empty($appConfig['secret'])) {
+        if (empty($appConfig['id']) || empty($appConfig['secret'])) {
             return new \WP_Error('no_config', 'No config');
         }
 
         $url = 'https://oauth.yandex.ru/authorize';
 
+        $state = [
+            'redirect' => $_GET['redirect'] ?? '',
+        ];
+
         $url = add_query_arg([
             'response_type' => 'code',
+            'state' => json_encode($state),
             'client_id' => $appConfig['id'],
         ], $url);
 
-        if(empty($_GET['code'])){
+        // request code
+        if (empty($_GET['code'])) {
             wp_redirect($url);
             exit;
         }
 
         $code = $_GET['code'] ?? null;
-        if(empty($code)){    
+        if (empty($code)) {
             return new \WP_Error('no_code', 'No code');
         }
 
-        $url = 'https://oauth.yandex.ru';
+        $url = 'https://oauth.yandex.ru/token';
 
-        $url = add_query_arg([
-            'code' => $code,
-            'grant_type' => 'authorization_code',
-        ], $url);
-
-
-        // var_dump($url); exit;
-
-        $data = wp_remote_request($url, [
+        $response = wp_remote_request($url, [
             'method' => 'POST',
-            'headers' => [
-                'Content-Type' => 'application/json',
-                // 'Content-Length ' => strlen(http_build_query([
-                //     'grant_type' => 'authorization_code',
-                //     'code' => $code,
-                // ])),
-                // 'Authorization' => 'Basic ' . base64_encode($appConfig['id'] . ':' . $appConfig['secret']),
-                // 'grant_type' => 'authorization_code',
-                // 'code' => $code,
+            'body' => [
+                'code' => $code,
+                'grant_type' => 'authorization_code',
             ],
-            
+            'headers' => [
+                'Content-type' => 'application/x-www-form-urlencoded',
+                'Authorization' => 'Basic ' . base64_encode($appConfig['id'] . ':' . $appConfig['secret']),
+            ],
+
         ]);
 
-        echo '<pre>';
-        var_dump($data); exit;
+        if (is_wp_error($response)) {
+            return $response;
+        }
 
-        $url = add_query_arg('', $req->get_param(''), $url);
-        // var_dump($req); exit;
+        $token_data = json_decode(wp_remote_retrieve_body($response), true);
+
+        $access_token = $token_data['access_token'] ?? null;
+        $refresh_token = $token_data['refresh_token'] ?? null;
+        $expires_in = $token_data['expires_in'] ?? null;
+
+        // @link https://wpcraft.ru/wp-json/socialify/v1/yandex
+
+        if(empty($access_token)) {
+            return new \WP_Error('no_access_token', 'No access token');
+        }
+
+        $userData = self::get_user_data($access_token);
+
         
+
+        $email = $userData['default_email'] ?? null;
+        if (empty($email)) {
+            return new \WP_Error('no_email', 'No email');
+        }
+
+        $user = get_user_by('email', $email);
+        if (empty($user)) {
+            $username = wp_generate_uuid4();
+            $user_id = wp_create_user($username, wp_generate_password(), $email);
+
+            $user = get_user_by('id', $user_id);
+
+            wp_update_user([
+                'ID' => $user_id,
+                'display_name' => $userData['display_name'] ?? $user->display_name,
+                'first_name' => $userData['first_name'] ?? $user->first_name,
+                'last_name' => $userData['last_name'] ?? $user->last_name,
+            ]);
+        }
+
+        
+        $meta = get_user_meta($user->ID, 'socialify', true);
+        if($meta){
+            $meta = [];
+        }
+        // $meta['yandex'] = [
+        //     'access_token' => $access_token,
+        //     'refresh_token' => $refresh_token,
+        //     'expires_in' => $expires_in,
+        // ];
+
+        update_user_meta($user->ID, 'socialify', $meta);
+
+        General::auth_user($user);
+
+        $state = json_decode($_GET['state'] ?? '', true);
+
+        $redirect_url = site_url();
+        if(isset($state['redirect']) && filter_var($state['redirect'], FILTER_VALIDATE_URL)) {
+            $redirect_url = $state['redirect'];
+        }
+
+        wp_redirect($redirect_url);
+        exit;
     }
 
-    
+    public static function get_user_data(string $access_token)
+    {
+        $url = 'https://login.yandex.ru/info';
+        $url = add_query_arg('format', 'json', $url);
+
+        $response = wp_remote_get($url, [
+            'headers' => [
+                'Authorization' => 'OAuth ' . $access_token,
+            ],
+        ]);
+
+        return json_decode(wp_remote_retrieve_body($response), true);
+    }
+
     /**
      * Add settings
      */
