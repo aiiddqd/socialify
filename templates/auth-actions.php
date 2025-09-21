750da63e08ae2e74f26adad6f2889166df46f52f
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-09-21 13:46:43 +0300

    feat: update social provider styles and structure; enhance button design and layout

--- templates/auth-actions.php
+++ templates/auth-actions.php
@@ -9,26 +9,14 @@ if (empty($items)) {
 
 ?>
 
-<div class="socialify-connect-providers">
+<div class="socialify-auth-providers">
     <?php foreach ($items as $key => $item) : ?>
-        <div
-            class="wp-block-group is-content-justification-space-between is-nowrap is-layout-flex wp-block-group-is-layout-flex">
-            <div class="wp-block-group is-nowrap is-layout-flex wp-block-group-is-layout-flex">
-                <figure class="wp-block-image size-full is-resized">
-                    <img decoding="async" width="64" height="64" alt="<?php echo esc_attr(ucfirst($key)); ?>"
-                        src="<?php echo esc_url($item['logo_url']); ?>" />
-                </figure>
-                <p>Продолжить с <span><?php echo esc_html($item['name']); ?></span></p>
-            </div>
-
-            <div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
-                <div class="wp-block-button">
-                    <a href="<?php echo esc_url($item['actionUrl']); ?>"
-                        class="wp-block-button__link wp-element-button socialify-provider-<?php echo esc_attr($key); ?>">
-                        <span><?= __('Вход', 'socialify') ?></span>
-                    </a>
-                </div>
-            </div>
-        </div>
+        <a class="socialify-btn" href="<?php echo esc_url($item['actionUrl']); ?>">
+            <figure >
+                <img width="16" height="16" decoding="async" alt="<?php echo esc_attr(ucfirst($key)); ?>"
+                    src="<?php echo esc_url($item['logo_url']); ?>" />
+            </figure>
+            <span>Продолжить с <span><?php echo esc_html($item['name']); ?></span></span>
+        </a>
     <?php endforeach; ?>
 </div>
